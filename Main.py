4cc5a543156aa9cd5f5ba37e26add4c2ca754255
Clebson1094 <clebson167.cj@gmail.com>
2025-08-05 13:53:50 -0300

    Adicionado Comentários
    Após assistir alguns minutos de um curso, resolvi adicionar comentários a essa aplicação simples (NO MOMENTO FOI UMA DAS MELHORES QUE CRIEI, ESPERO ACHAR SIMPLES  NO FUTURO).

--- Main.py
+++ Main.py
@@ -1,35 +1,40 @@
 from config import Calculator
+#Está importando a Classe Calculator do arquivo config
 
 calc = Calculator() #chamando a classe
 
 while True:
+    #Iniciando o Loop infinito(Ou quase infinito)
     print((("[1] Finance Control\n"
     "[2] Dream Planning\n"
     "[3] Show Table\n"
     "[4] Remove regisered debt\n"
     "[5] Exit\n")))
+    #Um menu de interatividade com o usuário em terminal
     try:
-        
+        #O try serve para evitar erros advindo dos inputs
         choices = int(input("Choice you option: "))
         if choices not in [1, 2, 3, 4, 5]:
             print("Please, choose a valid option: ")
             continue
+        #Nesse bloco ele define qual vai ser a escolha do usuário
         if choices == 2 and calc.balance == 0:
             print("I need to know about your finances first 😢")
             continue
-        
+        #Se escolher 2 e não tiver nada no balance[], ele não executa
         match choices:
-            
+            #Aqui abaixo em cada case está a escolha do usuário
             case 5:
                 print("Thanks for using my application")
                 break
-            
+            #Aqui ele finaliza o loop
             case 1:
                 nome = str(input("Enter name "))
                 if nome not in calc.entry and nome not in calc.exit:
                     option = str(input(f"{nome} is ENTRY or EXIT? ")).upper().strip()
                     if option == "ENTRY" or option == "EXIT":                       
                         calc.cadastro(nome, option)
+                        #Aqui se não tiver cadastrado o débito como saída ou entrada, ele irá cadastrar
                         print(f"{nome} Registration completed successfully in {option}")
                     else:
                         print("Invalid option.")
@@ -40,24 +45,32 @@ while True:
                     continue
                 calc.debt(nome, valor)
                 print("Successfully registered")
+                #No momento está bastante bagunçado e isso me incomoda, porém está que digamos "funcional"
+                #Mas ele executa a lógica principal que é cadastrar dentro da lista, e definir também se é entrada ou saída
             case 2:
                 value_dream = float(input("Enter value you dream"))
                 months = int(input("For months?"))
                 calc.planning_dream(value_dream, months, calc.balance)
-            
+                #Executa a lógica do planejando um sonho
             case 3:
                 calc.show_table()
                 continue
-            
+                #Função simples para printar a tabela e o usuário não se perder
             case 4:
                 if bool(calc.debts) == True:
+                    #Gosto de booleano
                     nome = str(input("Enter name to remove: "))
                     calc.remove_debt(nome)
+                    #Remove algum débito da lista
+                    if calc.correct == True:
+                        print(f"{nome} removed successfully")
                     continue
                 else:
                     print("No items in your financial table 😢")
+                    #Se o item não existir na lista, ele retorna para o menu
                     continue
     
     except ValueError:
         print("Please, choice a valid option")
+        #Caso o usuário faça algo que não foi solicitado EX: Colocar STR em INT ou FLOAT
         continue
--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 class Calculator():
-
-    def __init__(self):
+#Temos a majestosa classe
+    def __init__(self):#Aqui onde tudo inicia independente da ordem que ocorre as funções (EU ACHO)
         self.balance = 0
         self.debts = []
         self.entry = ["SALARIO", "COMISSOES"]
         self.exit = ["ALUGUEL", "CARTAO"]
 
-    def debt(self, nome, valor):
+    def debt(self, nome, valor): #Essa função basicamente irá identificar o que é entrar e somar, ou saída subtrair
         if nome in self.entry:
             tipo = "ENTRY"
         elif nome in self.exit:
@@ -14,34 +14,34 @@ class Calculator():
         else:
             return self.balance, self.debts  # Não faz nada se não encontrar
 
-        self.debts.append({"name": nome, "type": tipo, "value": valor})
+        self.debts.append({"name": nome, "type": tipo, "value": valor}) #Aqui é a estrutura do dicionário
         match tipo:
             case "ENTRY":
-                self.balance += valor
+                self.balance += valor #Se identificar como entrada soma
             case "EXIT":
-                self.balance -= valor
-        return self.balance, self.debts
+                self.balance -= valor #Se identificar como saída subtrai
+        return self.balance, self.debts #Se não for entrada nem saída, não faz nada
 
-    def planning_dream(self, value_dream, months, balance):
-        real_dream = value_dream / months
-        recommendation = value_dream / (balance * 30 / 100)
-        if recommendation > real_dream:
+    def planning_dream(self, value_dream, months, balance): #Esse é um conceito muito bom de plano para o futuro, porém irei melhorar 200% dessa lógica
+        real_dream = value_dream / months #Aqui pega o valor do seu sonho e divide pelos meses que você quer conquistar
+        recommendation = value_dream / (balance * 30 / 100) #Aqui serve para ser uma alternativa melhor, no caso fazer um planejamento seguro, pegando apenas 30% do seu saldo liquido mensal
+        if recommendation > real_dream:#Se ele recomendar um valor maior em menos prazo, te mostra
             print(f"To reach you goal {real_dream:.2f} for month")
             print(f"For safety i recommend {balance * 30 / 100:.2f} for month, during {recommendation:.2f}")
         else:
-            print(f"To reach you goal {real_dream:.2f} for month")
+            print(f"To reach you goal {real_dream:.2f} for month") #Caso contrario, somente o valor bruto mesmo
 
-    def show_table(self):
-        if bool(self.debts) == True:
+    def show_table(self): #Essa função foi meu brother GUANABARA PAI DA CIÊNCIA que me ensinou, utilizando um FOR para printar meu dicionário
+        if bool(self.debts) == True: #Eu gosto de booleano
             print("TABLE")
             for debt in self.debts:
                 print(f"Name: {debt['name']} | Type: {debt['type']} | Value: R$:{debt['value']:.2f}")
             print(f"Final Balance: {self.balance:.2f} ")
             return self.balance, self.debts
-        else:
+        else: #Se a lista de dicionários tiver vazia, printa isso
             print("No items in your financial table 😢")
 
-    def cadastro(self, nome, option):
+    def cadastro(self, nome, option): #Essa função cadastra o nome e o tipo de entrada ou saída financeira que você tem
         if option == "ENTRY":
             tipo = "ENTRY"
             self.entry.append(nome)
@@ -50,16 +50,14 @@ class Calculator():
             tipo = "EXIT"
             self.exit.append(nome) 
             return tipo
-        return self.exit, self.entry
+        return self.exit, self.entry #Se não for entrada nem saída, retorna nadinha
 
-    def remove_debt(self, nome):
+    def remove_debt(self, nome): #Aqui removemos os débitos cadastrados e ainda devolvemos o valor devolta ao saldo
         for debt in self.debts:
             if debt['name'] == nome and debt['type'] == "ENTRY":
                 self.balance -= debt['value']
-                print(f"{nome} removed successfully")
             elif debt['name'] == nome and debt['type'] == "EXIT":
                 self.balance += debt['value']
-                print(f"{nome} removed successfully")
             self.debts.remove(debt)
         else:
             print(f"{nome} not found in your debts")
